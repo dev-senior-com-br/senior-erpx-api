21ceb6ae9958907411158dd5886fa1c3cdb4d2d5
Emerson Kopp <15053421+emersonkopp@users.noreply.github.com>
2021-12-02 09:36:38 -0300

    Added Product payload.

---
+++ src/main/java/br/com/senior/erpx/ProductPayload.java
@@ -0,0 +1,130 @@
+package br.com.senior.erpx;
+
+import org.apache.camel.component.jackson.JacksonDataFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(serialization = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductPayload {
+
+    public static final JacksonDataFormat PRODUCT_FORMAT = new JacksonDataFormat(ProductPayload.class);
+
+    /**
+     * Id da entidade
+     */
+    public String id;
+
+    /**
+     * Empresa
+     */
+    // public Company company;
+
+    /**
+     * Família
+     */
+    // public Family family;
+
+    /**
+     * Código
+     */
+    public String code;
+
+    /**
+     * Descrição
+     */
+    public String description;
+
+    /**
+     * Descrição complementar
+     */
+    public String additionalDescription;
+
+    /**
+     * Código de barras
+     */
+    public String barcode;
+
+    /**
+     * Código barras livre
+     */
+    public String freeBarcode;
+
+    /**
+     * GTIN unidade tributável
+     */
+    public String gtin;
+
+    /**
+     * Identificação produto
+     */
+    public String productIdentification;
+
+    /**
+     * Peso bruto
+     */
+    public Double grossWeight;
+
+    /**
+     * Peso líquido
+     */
+    public Double netWeight;
+
+    /**
+     * Código da Referência
+     */
+    public String referenceCode;
+
+    /**
+     * Natureza de gasto
+     */
+    // public SpendingNature spendingNature;
+
+    /**
+     * Situação
+     */
+    public String status;
+
+    /**
+     * Motivo da situação
+     */
+    // public StatusReason statusReason;
+
+    /**
+     * Enquadramento de produto específico
+     */
+    public String productFramework;
+
+    /**
+     * Indica se o registro foi excluído
+     */
+    public Boolean deleted;
+
+    /**
+     * Dados comerciais do produto
+     */
+    // public CommercialProduct commercialProduct;
+
+    /**
+     * Dados fiscais do produto
+     */
+    // public TaxProduct taxProduct;
+
+    /**
+     * Dados de estoque do produto
+     */
+    // public StockProduct stockProduct;
+
+    /**
+     * Dados de compra do produto
+     */
+    // public PurchaseProduct purchaseProduct;
+
+    /**
+     * Dados de rateio financeiro do produto
+     */
+    // public ApportionmentProduct apportionmentProduct;
+
+}
